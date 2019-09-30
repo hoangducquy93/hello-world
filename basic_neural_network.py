a94d3844a12f8c79c021888e844aef4fa44b2735
D.Q.Hoang <54616267+hoangducquy93@users.noreply.github.com>
2019-09-30 13:43:19 +0700

    Add files via upload

---
+++ basic_neural_network.py
@@ -0,0 +1,8 @@
+import numpy as np 
+
+training_set_inputs = np.array([[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0]])
+training_set_outputs = np.array([[0,0,0,1,0,1,1]]).T
+
+#print(training_set_inputs)
+print(training_set_outputs)
+
